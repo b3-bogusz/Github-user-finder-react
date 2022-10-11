532d18553e4aee0b5bb03f9902ee89350cef2975
p.bogusz <p.bogusz@cheil.com>
2022-10-11 21:27:23 +0200

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div className="bg-purple-100">
-      <h1 className="text-3xl">Hello world</h1>
-      <button className="btn">click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
