7b219ec42cb40afcbe3ca287abde074366ba1f28
p.bogusz <p.bogusz@cheil.com>
2022-10-30 21:06:44 +0100

    reafctoring getUser and getRepos actions

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,8 +4,7 @@ import GithubContext from "../context/github/GithubContext";
 import { useParams, Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
-import { getUser } from "../context/github/GithubActions";
-import { getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -14,10 +13,8 @@ function User() {
   useEffect(() => {
     dispatch({ type: "SET_LOADING" });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: "GET_USER", payload: userData });
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: "GET_REPOS", payload: userRepoData });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
     };
     getUserData();
   }, [dispatch, params.login]);
