ae9a7fb2eb43e3408bdc4ad7ba399a29fe5e5cdc
p.bogusz <p.bogusz@cheil.com>
2022-10-30 19:39:37 +0100

    display user profile inforamtion and repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -62,6 +63,34 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  //Get repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+      dispatch({
+        type: "GET_REPOS",
+        payload: data,
+      });
+    }
+  };
+
   //Set loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
@@ -70,10 +99,12 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         user: state.user,
+        repos: state.repos,
         loading: state.loading,
         searchUsers,
         deleteUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
